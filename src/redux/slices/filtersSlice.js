6bfc88e7084453a62eb4923a0b465a9d65f43aea
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-10 18:34:54 +0100

    chore: setup and configure eslint & refactor: apply lint:fix

--- src/redux/slices/filtersSlice.js
+++ src/redux/slices/filtersSlice.js
@@ -1,17 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   filters: {
     isApply: false,
-  }
-}
+  },
+};
 
 const filtersSlice = createSlice({
   name: 'filters',
   initialState,
   reducers: {
-  }
-})
+  },
+});
 
 export const filtersActions = filtersSlice.actions;
 export const filtersReducer = filtersSlice.reducer;
