7e028e62672325c5139767df36d3534d963cc8e6
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-10 22:21:40 +0100

    feat: add normalize.scss, change root to body,  import fonts, basic global stiles

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global.scss';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.body);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,5 @@
+import style from './main-page.module.scss';
+
+export default function MainPage() {
+  return <div className={style['main-page']}>Main page</div>;
+}
