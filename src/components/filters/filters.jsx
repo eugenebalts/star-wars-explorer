62adaa5c54ff64bb3a32d3c26d70f64687e25d10
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-11 11:16:59 +0100

    feat: put filters in container

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -1,5 +1,31 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import { AccordionActions, AccordionDetails, Typography } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import style from './filters.module.scss';
+import FilterActions from './actions/actions';
+import FilmsFilter from './films/films-filter';
 
 export default function Filters() {
-  return <div className={style.filters}>Filters</div>;
+  return (
+    <Accordion className={style.filters}>
+      <AccordionSummary
+        expandIcon={<ArrowDropDownIcon className={style['filters__dropdown-icon']} />}
+        aria-controls="panel1-content"
+        id="panel1-header">
+        <Typography className={style.filters__select}>Filters</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <div className={style.filters__filters}>
+          <div>
+            <FilmsFilter />
+          </div>
+          <div>A</div>
+        </div>
+        <AccordionActions>
+          <FilterActions />
+        </AccordionActions>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
