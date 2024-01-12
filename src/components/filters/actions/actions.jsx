a2609338047b71dae05b877dc30b3d30ac13a3fd
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-12 08:04:43 +0100

    feat: disabled filters btn depending on the filters changing

--- src/components/filters/actions/actions.jsx
+++ src/components/filters/actions/actions.jsx
@@ -1,18 +1,29 @@
 import Button from '@mui/material/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { filtersActions } from '../../../redux/slices/filtersSlice';
 import style from './actions.module.scss';
 
 export default function FilterActions() {
   const dispatch = useDispatch();
   const { resetFilters } = filtersActions;
+  const { isChanged } = useSelector((state) => state.filters);
 
   const handleButtonClick = () => {
     dispatch(resetFilters());
   };
 
+  useEffect(() => {
+    console.log(isChanged);
+  }, [isChanged]);
+
   return (
-    <Button variant="contained" color="primary" onClick={handleButtonClick} className={style.actions__button}>
+    <Button
+      disabled={!isChanged}
+      variant="contained"
+      color="primary"
+      onClick={handleButtonClick}
+      className={style.actions__button}>
       Reset
     </Button>
   );
--- src/redux/slices/filtersSlice.js
+++ src/redux/slices/filtersSlice.js
@@ -10,6 +10,7 @@ const initialState = {
   gender: '',
   films: [],
   page: 1,
+  isChanged: false,
 };
 
 const filtersSlice = createSlice({
@@ -17,25 +18,45 @@ const filtersSlice = createSlice({
   initialState,
   reducers: {
     updateSearch(state, { payload }) {
-      state.search = payload;
+      if (state.search !== payload) {
+        state.search = payload;
+        state.isChanged = true;
+      }
     },
     updateMass(state, { payload }) {
-      state.mass = payload;
+      if (state.mass !== payload) {
+        state.mass = payload;
+        state.isChanged = true;
+      }
     },
     updateGender(state, { payload }) {
-      state.gender = payload;
+      if (state.gender !== payload) {
+        state.gender = payload;
+        state.isChanged = true;
+      }
     },
     updateFilms(state, { payload }) {
-      state.films = [...payload];
+      if (state.films.toString() !== payload.toString()) {
+        state.films = [...payload];
+        state.isChanged = true;
+      }
     },
     updatePage(state, { payload }) {
-      state.page = payload;
+      if (state.page !== payload) {
+        state.page = payload;
+      }
     },
     updateMinMass(state, { payload }) {
-      state.minMass = payload;
+      if (state.minMass !== payload) {
+        state.minMass = payload;
+        state.isChanged = true;
+      }
     },
     updateMaxMass(state, { payload }) {
-      state.maxMass = payload;
+      if (state.maxMass !== payload) {
+        state.maxMass = payload;
+        state.isChanged = true;
+      }
     },
     resetFilters(state) {
       state.search = '';
@@ -45,6 +66,7 @@ const filtersSlice = createSlice({
       state.mass = '';
       state.gender = '';
       state.films = [];
+      state.isChanged = false;
     },
   },
 });
