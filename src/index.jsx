5bd289b851b319517e0349f0cb88bc1d6e93dd50
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-15 02:33:29 +0100

    fix: root error, crumbs key err

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.body);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
