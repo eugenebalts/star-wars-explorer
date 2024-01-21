b5f79067d03fd19fca73df3ece2652291698a3e7
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-01-21 22:48:23 +0100

    refactor: replace character-fetches to characterSlice using createAsyncThunk

--- src/pages/character-page/character-page.jsx
+++ src/pages/character-page/character-page.jsx
@@ -1,11 +1,17 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import style from './character-page.module.scss';
 import pageStyle from '../page.module.scss';
-import api from '../../services/api';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Image from '../../assets/person.png';
-import getIdFromUrl from '../../utils/getIdFromUrl';
 import CustomBreadcrumbs from '../../components/breadcrumbs/breadcrumbs';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  characterActions,
+  fetchCharacter,
+  fetchSpecies,
+  fetchStarships,
+  fetchFilms,
+} from '../../redux/slices/characterSlice';
 
 const breadcrumbLink = [
   {
@@ -17,105 +23,45 @@ const breadcrumbLink = [
 export default function CharacterPage() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [cardData, setCardData] = useState({});
-  const [films, setFilms] = useState([]);
-  const [species, setSpecies] = useState([]);
-  const [starships, setStarships] = useState([]);
-
-  const fetchCharacter = async () => {
-    try {
-      const response = await api.getCharacter(id);
-
-      setCardData(response);
-    } catch (err) {
-      navigate('/not-found');
-    }
-  };
-
-  const fetchSpecies = async () => {
-    const updatedSpecies = [];
-
-    await Promise.all(
-      cardData.species.map(async (url) => {
-        const speciesId = getIdFromUrl(url, 'species')[0];
-
-        try {
-          const response = await api.getSpeciesById(speciesId);
-          updatedSpecies.push(response.name);
-        } catch (err) {
-          console.error(`Cannot fetch species for id ${speciesId} with error: ${err}`);
-        }
-      })
-    );
-
-    setSpecies(updatedSpecies);
-  };
-
-  const fetchFilms = async () => {
-    const newFilms = [];
-
-    await Promise.all(
-      cardData.films.map(async (url) => {
-        const filmId = getIdFromUrl(url, 'films')[0];
-
-        try {
-          const response = await api.getFilmById(filmId);
-          newFilms.push(response.title);
-        } catch (err) {
-          console.error(`Cannot fetch films for id ${filmId} with error: ${err}`);
-        }
-      })
-    );
-
-    setFilms(newFilms);
-  };
-
-  const fetchStarships = async () => {
-    const newStarships = [];
-
-    await Promise.all(
-      cardData.starships.map(async (url) => {
-        const starshipId = getIdFromUrl(url, 'starships')[0];
-
-        try {
-          const response = await api.getStarshipById(starshipId);
-          newStarships.push(response.model);
-        } catch (err) {
-          console.error(`Cannot fetch starships for id ${starshipId} with error: ${err}`);
-        }
-      })
-    );
-
-    setStarships(newStarships);
-  };
+  const { resetCharacter } = characterActions;
+  const dispatch = useDispatch();
+  const { characterData, species, starships, films, error } = useSelector((state) => state.character);
 
   useEffect(() => {
-    if (cardData?.species?.length) fetchSpecies();
-    if (cardData?.films?.length) fetchFilms();
-    if (cardData?.starships?.length) fetchStarships();
-  }, [cardData]);
+    dispatch(fetchCharacter(id));
 
-  useEffect(() => {
-    fetchCharacter();
+    return () => {
+      dispatch(resetCharacter());
+    };
   }, []);
+
+  useEffect(() => {
+    dispatch(fetchSpecies(characterData.species));
+    dispatch(fetchStarships(characterData.starships));
+    dispatch(fetchFilms(characterData.films));
+  }, [characterData]);
+
+  useEffect(() => {
+    if (error) navigate('/not-found');
+  }, [error]);
 
   return (
     <div className={`${style['character-page']} ${pageStyle.page}`}>
-      <CustomBreadcrumbs links={breadcrumbLink} currentPage={cardData?.name ?? 'Character'} />
+      <CustomBreadcrumbs links={breadcrumbLink} currentPage={characterData?.name ?? 'Character'} />
       <section className={`${pageStyle.section}`}>
         <div className={style['character-about']}>
-          <h2 className={style['character-about__title']}>{cardData?.name || 'unnamed'}</h2>
+          <h2 className={style['character-about__title']}>{characterData?.name || 'unnamed'}</h2>
           <div className={style['character-about__introduction']}>
             <div className={style['character-about__introduction__image']}>
               <img src={Image} alt="character" />
             </div>
             <div className={style['character-about__introduction__about ']}>
               <p className={style['character-about__introduction__summary']}>
-                {cardData?.name ?? 'This'} is a character from the iconic Star Wars universe. Born{' '}
-                {cardData?.birth_year ? `in ${cardData.birth_year}` : ' a long time ago'}, standing at a height of{' '}
-                {cardData?.birth_year ?? 'height of an average person'} in cm and weighing{' '}
-                {cardData?.mass ?? 'something between an average values of usual people'} kg. Characters skin color is{' '}
-                {cardData?.skin_color ?? 'very beautiful'}, and {cardData?.gender ?? 'unknown'} gender.
+                {characterData?.name ?? 'This'} is a character from the iconic Star Wars universe. Born{' '}
+                {characterData?.birth_year ? `in ${characterData.birth_year}` : ' a long time ago'}, standing at a
+                height of {characterData?.birth_year ?? 'height of an average person'} in cm and weighing{' '}
+                {characterData?.mass ?? 'something between an average values of usual people'} kg. Characters skin color
+                is {characterData?.skin_color ?? 'very beautiful'}, and {characterData?.gender ?? 'unknown'} gender.
               </p>
             </div>
           </div>
